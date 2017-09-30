471a2df07e1d51bfa7bb33161273f1d19e9bfb48
Rahul <rahulr56@users.noreply.github.com>
2017-09-29 20:37:48 -0400

    Add files via upload
    Bash Script to run the python file

---
+++ predict.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "------------------------------------------------------------------------------------"
+echo "-                Computing the Models with a Smoothing Value of 0                  -"
+echo "------------------------------------------------------------------------------------"
+echo ""
+echo "Running Language predictor with a smoothing value of 0"
+./Rachapalli_Assignment2.py 0
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "------------------------------------------------------------------------------------"
+echo "-          Recomputing the Models with a Smoothing Value of 0.000001               -"
+echo "------------------------------------------------------------------------------------"
+echo ""
+echo "Running Language predictor with a smoothing value of 0.000001"
+./Rachapalli_Assignment2.py
